1380b8c2c9ea25dbcf17b6c4a9de1cc52fe854a6
Rafael-Felipe-RF <rafaelicm18@gmail.com>
2025-02-14 22:25:38 -0300

    Initial commit

---
+++ java.js
@@ -0,0 +1,21 @@
+var menuitem = document.querySelectorAll('.item-menu');
+
+function selectlink() {
+    menuitem.forEach((item) => 
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuitem.forEach((item) => 
+    item.addEventListener('click', selectlink)
+)
+
+//expandir o menu
+
+var btnExp = document.querySelector('.btn-exp')
+var menuSide = document.querySelector('.menu-side')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
